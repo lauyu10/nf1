f07b69b120e572fb0a3c74ff329417646602cfe4
Laurent Yu <laurentyu1997@hotmail.fr>
2018-02-20 14:53:25 +0100

    test de l'implémentation de jquery sur les pages tpl

--- view/js/script.js
+++ view/js/script.js
@@ -1,4 +1,12 @@
 /*
+$('p'); // je sélectionne tous les paragraphes
+
+$('.maClasse'); // je sélectionne les éléments ayant .maClasse pour classe
+
+$('#monId'); // je sélectionne l'élément qui possède l'identifiant #monId
+*/
+
+/*
 function display_hello()
 {
   alert("hello world");
