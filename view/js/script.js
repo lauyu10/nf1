c77cd294323ad374978691af8020a0fa71a50982
Laurent Yu <laurentyu1997@hotmail.fr>
2018-02-23 10:06:26 +0100

    vérification des notes prises depuis le cours openclassroom

--- view/js/script.js
+++ view/js/script.js
@@ -4,6 +4,9 @@ $('p'); // je sélectionne tous les paragraphes
 $('.maClasse'); // je sélectionne les éléments ayant .maClasse pour classe
 
 $('#monId'); // je sélectionne l'élément qui possède l'identifiant #monId
+
+jQuery(document).ready(function(){}); = $(document).ready(function(){}); = $(function(){});
+
 */
 
 /*
